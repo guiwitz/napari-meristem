1234ff48cb54346858adbe83de02758f15348699
guiwitz <guiwitz@users.noreply.github.com>
2025-06-05 10:44:40 +0200

    option to edit track, clean-up

--- src/napari_meristem/meristem_select_widget.py
+++ src/napari_meristem/meristem_select_widget.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import napari
 from natsort import natsorted
+from warnings import warn
 import skimage
 import numpy as np
 import pandas as pd
@@ -48,11 +49,6 @@ class MeristemAnalyseWidget(QWidget):
         self.data_selection_group.glayout.addWidget(self.manual_option.native, 2, 0, 1, 2)
         self.manual_option.value = False
 
-        '''self.btn_load_tracked = QPushButton("Load tracked data")
-        self.data_selection_group.glayout.addWidget(self.btn_load_tracked, 2, 0, 1, 2)
-        self.btn_load_tracked.setToolTip("Load tracked data from the selected folder")
-        self.btn_load_tracked.clicked.connect(self.load_tracks)'''
-
         # Handles complexes
         self.pick_group = VHGroup('Complexes', orientation='G')
         self.tabs.add_named_tab('Tracking', self.pick_group.gbox)
@@ -66,22 +62,12 @@ class MeristemAnalyseWidget(QWidget):
         self.pick_group.glayout.addWidget(self.btn_new_complex, 2, 0, 1, 2)
         self.btn_new_complex.setToolTip("Create a new complex")
         
-        """
-        self.btn_extract = QPushButton('Extract')
-        self.pick_group.glayout.addWidget(self.btn_extract, 0, 0, 1, 2)
-
-        self.qlist_cells = QListWidget()
-        self.pick_group.glayout.addWidget(self.qlist_cells, 1, 0, 1, 2)
-        self.btn_remove_cell = QPushButton('Remove selected cell')
-        self.pick_group.glayout.addWidget(self.btn_remove_cell, 2, 0, 1, 2)
-        self.btn_remove_cell.clicked.connect(self._on_remove_cell)
-        """
         
         # Create tree manually
         self.mtrack_group = VHGroup('Manual tracking', orientation='G')
         self.tabs.add_named_tab('Tracking', self.mtrack_group.gbox)
-        self.btn_add_to_track = QPushButton('Initialize tracking')
-        self.mtrack_group.glayout.addWidget(self.btn_add_to_track, 0, 0, 1, 2)
+        self.btn_initialize_tracking = QPushButton('Initialize tracking')
+        self.mtrack_group.glayout.addWidget(self.btn_initialize_tracking, 0, 0, 1, 2)
         self.btn_new_track = QPushButton('New track')
         self.mtrack_group.glayout.addWidget(self.btn_new_track, 1, 0, 1, 1)
         self.btn_new_track.setToolTip("Start a new track")
@@ -90,14 +76,21 @@ class MeristemAnalyseWidget(QWidget):
         self.btn_remove_track.setToolTip("Remove the selected track")
 
         self.track_list = QListWidget()
-        self.mtrack_group.glayout.addWidget(self.track_list, 2, 0, 1, 2)
+        self.mtrack_group.glayout.addWidget(self.track_list, 2, 0, 1, 1)
         self.track_list.setToolTip("List of tracks")
         self.track_list.setSelectionMode(QListWidget.MultiSelection)
 
+        self.track_sequence_list = QListWidget()
+        self.mtrack_group.glayout.addWidget(self.track_sequence_list, 2, 1, 1, 1)
+        self.track_sequence_list.setToolTip("List of track parts")
+
         self.btn_set_mother_daughter = QPushButton('Set mother-daughter')
-        self.mtrack_group.glayout.addWidget(self.btn_set_mother_daughter, 3, 0, 1, 2)
+        self.mtrack_group.glayout.addWidget(self.btn_set_mother_daughter, 3, 0, 1, 1)
         self.btn_set_mother_daughter.setToolTip("Set mother-daughter relationship for the selected cells")
-        self.btn_set_mother_daughter.clicked.connect(self._on_mother_daughter)
+        
+
+        self.btn_remove_track_position = QPushButton('Remove track position')
+        self.mtrack_group.glayout.addWidget(self.btn_remove_track_position, 3, 1, 1, 1)
 
         self.btn_export_tracks = QPushButton('Export tracks')
         self.mtrack_group.glayout.addWidget(self.btn_export_tracks, 4, 0, 1, 1)
@@ -127,18 +120,20 @@ class MeristemAnalyseWidget(QWidget):
         self.identity_group.glayout.addWidget(self.btn_import_identities, 4, 1, 1, 1)
         self.btn_import_identities.setToolTip("Import identities from a file")
         
-        
         self._add_connections()
 
     def _add_connections(self):
         
         self.btn_load_warped.clicked.connect(self._on_load_warped_data)
-        #self.btn_extract.clicked.connect(self.extract_cells)
-        self.btn_add_to_track.clicked.connect(self.add_track_layer)
+        self.btn_initialize_tracking.clicked.connect(self.add_track_layer)
         self.btn_new_track.clicked.connect(self._on_new_track)
         self.btn_remove_track.clicked.connect(self._on_remove_track)
         self.btn_export_tracks.clicked.connect(self._on_export_tracks)
         self.btn_import_tracks.clicked.connect(self._on_import_tracks)
+        self.track_list.itemSelectionChanged.connect(self._on_select_track)
+        self.btn_set_mother_daughter.clicked.connect(self._on_mother_daughter)
+        self.btn_remove_track_position.clicked.connect(self._on_remove_track_position)
+        self.track_sequence_list.itemSelectionChanged.connect(self._on_select_track_sequence)
 
         self.widget_export_directory.changed.connect(self._on_update_complex_list)
         self.btn_new_complex.clicked.connect(self._on_create_complex)
@@ -168,69 +163,6 @@ class MeristemAnalyseWidget(QWidget):
         #self.test = masks_warped.astype(np.uint16)
         self.viewer.add_labels(masks_warped.astype(np.uint16), name='warped_mask')
 
-    def extract_cells(self):
-
-        pass
-
-    '''def load_tracks(self):
-        
-        self._on_load_warped_data()
-        man_tracks = natsorted(Path(self.widget_export_directory.value).joinpath('tracked').glob('man_track*.tif'))
-        man_tracks = np.stack([skimage.io.imread(t) for t in man_tracks], axis=0)
-        self.viewer.add_labels(man_tracks, name='tracked_mask', blending='additive')
-
-        self.viewer.layers['tracked_mask'].mouse_drag_callbacks.append(self.get_label_value)'''
-
-    '''def get_label_value(self, layer, event):
-        # Convert click position to data coordinates
-        self.coordinates = event.position#layer.world_to_data(event.position)
-        self.coordinates = tuple(map(int, self.coordinates))
-
-        # Get value at the clicked coordinates
-        value = layer.data[self.coordinates] if all(0 <= c < s for c, s in zip(self.coordinates, layer.data.shape)) else None
-        if value is not None:
-            self.selected_cell_ids.append(value)
-            # Update the QListWidget with the selected cell ID
-            self.qlist_cells.addItem(f'{value}')
-            print(f"Selected cell IDs: {value}")
-
-            track_labels = pd.read_csv(self.widget_export_directory.value.joinpath('tracked', 'man_track.txt'), 
-            sep=' ', header=None, names=['id', 'birth_t','division_t', 'mother_id'])
-        
-            lineage_ids = [self.selected_cell_ids[-1]]
-            mother_id = track_labels[track_labels.id == self.selected_cell_ids[-1]].mother_id.values[0]
-            while mother_id > 0:
-                new_id = track_labels[track_labels.id == mother_id].id.values[0]
-                lineage_ids.append(new_id)
-                mother_id = track_labels[track_labels.id == new_id].mother_id.values[0]
-
-            self.all_lineages.append(lineage_ids)
-
-            self.update_mask()'''
-
-    '''def update_mask(self):
-        
-        selected_cells = np.zeros_like(self.viewer.layers['tracked_mask'].data)
-        for lineage in self.all_lineages:
-            selected_cells += np.where(np.isin(self.viewer.layers['tracked_mask'].data, lineage), self.viewer.layers['tracked_mask'].data, 0)
-        if 'selected_cells' in self.viewer.layers:
-            self.viewer.layers['selected_cells'].data = selected_cells
-        else:
-            self.viewer.add_labels(selected_cells, name='selected_cells', blending='additive')'''
-
-    def _on_remove_cell(self):
-        # This method should handle the selection of cells from the list widget
-        selected_items = self.qlist_cells.selectedItems()
-        if selected_items:
-            selected_cell_row = int(selected_items[0].text())
-            
-            ind = self.selected_cell_ids.index(selected_cell_row)
-            self.selected_cell_ids.pop(ind)
-            self.all_lineages.pop(ind)
-            self.qlist_cells.takeItem(ind)
-            print(f"Removed cell ID: {selected_cell_row}")
-
-            self.update_mask()
 
     def add_track_layer(self):
         """Add a new track layer to the viewer."""
@@ -240,6 +172,7 @@ class MeristemAnalyseWidget(QWidget):
         self.current_track = None
         self.graph = {}
         self.track_list.clear()
+        self.track_sequence_list.clear()
 
         self.viewer.add_tracks([[0,0,0,0]],
             name='manual_track',
@@ -249,10 +182,10 @@ class MeristemAnalyseWidget(QWidget):
         self.track_id = 0
 
     def _on_add_to_track(self, layer, event):
-
+        """Callback when adding a point to the track when clicking on the "manual_track" layer."""
+        
         if (self.current_track is None):
             self.current_track = pd.DataFrame(columns=['track_id', 't', 'y', 'x', 'label', 'identity'])
-        
         self.coordinates = event.position#layer.world_to_data(event.position)
         self.coordinates = tuple(map(int, self.coordinates))
 
@@ -265,24 +198,16 @@ class MeristemAnalyseWidget(QWidget):
         self.track_id = int(current_track_id[0].text())
 
         if (self.current_track is None):
-            '''self.current_track = np.array([[
-                self.track_id,
-                self.coordinates[0],
-                self.coordinates[1],
-                self.coordinates[2],
-                label
-                ]])'''
+        
             self.current_track = pd.DataFrame(
                 [self.track_id, self.coordinates[0], self.coordinates[1],
-                self.coordinates[2], label, 'None'], columns=['track_id', 't', 'y', 'x', 'label'])
+                self.coordinates[2], label, 'None'], columns=['track_id', 't', 'y', 'x', 'label', 'identity'])
         else:
-            '''self.current_track = np.append(
-                self.current_track,
-                [[self.track_id, self.coordinates[0], self.coordinates[1], self.coordinates[2], label]],
-                axis=0)'''
-            self.current_track.loc[len(self.current_track)] = [self.track_id, self.coordinates[0], self.coordinates[1], self.coordinates[2], label, 'None']
-        '''self.current_track = self.current_track[
-            np.lexsort((self.current_track[:, 0], self.current_track[:, 1]))]'''
+            self.current_track = pd.concat(
+                [self.current_track,  
+                 pd.DataFrame([[self.track_id, self.coordinates[0], self.coordinates[1], self.coordinates[2], label, 'None']], columns=self.current_track.columns)]
+                 )
+        self.fix_track_dtypes()
         self.current_track = self.current_track.sort_values(by=['track_id', 't'])
 
         self.track_list.clear()
@@ -290,9 +215,56 @@ class MeristemAnalyseWidget(QWidget):
             self.track_list.addItem(f'{track}')
         # select back the self.track_id
         self.track_list.setCurrentItem(self.track_list.findItems(str(self.track_id), Qt.MatchExactly)[0])
+        self.update_track_sequence_list()
+        self.track_sequence_list.setCurrentItem(self.track_sequence_list.findItems(str(self.coordinates[0]), Qt.MatchExactly)[0])
 
         layer.data = self.current_track[['track_id', 't', 'y', 'x']].values
         layer.refresh()
+
+    def fix_track_dtypes(self):
+        
+        self.current_track.track_id = self.current_track.track_id.astype(int)
+        self.current_track.t = self.current_track.t.astype(int)
+        self.current_track.y = self.current_track.y.astype(int)
+        self.current_track.x = self.current_track.x.astype(int)
+        self.current_track.label = self.current_track.label.astype(int)
+        self.current_track.identity = self.current_track.identity.astype(str)
+        
+    def update_track_sequence_list(self):
+        # update the track sequence list
+        self.track_sequence_list.clear()
+        track_times = self.current_track[self.current_track.track_id == self.track_id].t
+        for t in track_times:
+            self.track_sequence_list.addItem(f'{t}')
+
+    def _on_select_track(self):
+        """Update the track sequence list when a track is selected."""
+        
+        selected_items = self.track_list.selectedItems()
+        if len(selected_items) == 0:
+            return
+        
+        selected_track_id = int(selected_items[0].text())
+        self.track_sequence_list.clear()
+
+        if len(selected_items) == 1:
+        
+            # get the times for the selected track
+            if self.current_track is not None:
+                track_times = self.current_track[self.current_track.track_id == selected_track_id].t
+                for t in track_times:
+                    self.track_sequence_list.addItem(f'{t}')
+                
+                if self.track_sequence_list.count() > 0:
+                    self.track_sequence_list.setCurrentItem(self.track_sequence_list.item(0))
+
+    
+    def _on_select_track_sequence(self):
+        """Upon selecting position in track sequence list, update the viewer's current step."""
+        
+        if len(self.track_sequence_list.selectedItems()) > 0:
+            current_item = int(self.track_sequence_list.selectedItems()[0].text())
+            self.viewer.dims.current_step = (current_item, *self.viewer.dims.current_step[1::])
 
     def _on_new_track(self):
         """Start a new track."""
@@ -321,6 +293,31 @@ class MeristemAnalyseWidget(QWidget):
             self.viewer.layers['manual_track'].graph = self.graph
             self.viewer.layers['manual_track'].refresh()
 
+    def _on_remove_track_position(self):
+        """Remove the selected track position."""
+        
+        selected_items = self.track_sequence_list.selectedItems()
+        if len(selected_items) == 0:
+            return
+        
+        current_track = int(self.track_list.selectedItems()[0].text())
+        current_trackpos = int(selected_items[0].text())
+        
+        # remove the position from the current track
+        if self.current_track is not None:
+            to_remove = (self.current_track.track_id == current_track) & (self.current_track.t == current_trackpos)
+            self.current_track = self.current_track[~to_remove]
+            if len(self.current_track) == 0:
+                track = [[0,0,0,0]]
+                self.track_sequence_list.clear()
+            else:
+                track = self.current_track[['track_id', 't', 'y', 'x']].values
+                self.update_track_sequence_list()
+            
+            self.viewer.layers['manual_track'].data = track
+            self.viewer.layers['manual_track'].refresh()
+
+
     def _on_remove_track(self):
         """Remove the selected track."""
         selected_items = self.track_list.selectedItems()
@@ -332,6 +329,7 @@ class MeristemAnalyseWidget(QWidget):
                 track = [[0,0,0,0]]
             else:
                 track = self.current_track[['track_id', 't', 'y', 'x']].values
+            self.track_sequence_list.clear()
             self.track_list.takeItem(self.track_list.row(selected_items[0]))
             self.viewer.layers['manual_track'].data = track
             self.viewer.layers['manual_track'].refresh()
@@ -382,8 +380,11 @@ class MeristemAnalyseWidget(QWidget):
 
         export_folder = Path(self.widget_export_directory.value)
         export_folder = export_folder.joinpath('complexes',f'complex_{int(self.complex_list.selectedItems()[0].text())}')
-          
         
+        if not export_folder.joinpath('manual_tracks.csv').exists():
+            warn(f"Manual tracks file not found in {export_folder}.")
+            return
+
         self.current_track = pd.read_csv(export_folder.joinpath('manual_tracks.csv'))
         if 'identity' not in self.current_track.columns:
             self.current_track['identity'] = 'None'
@@ -408,7 +409,8 @@ class MeristemAnalyseWidget(QWidget):
             self.complex_list.addItem(f'{complex_id}')
 
     def _on_create_complex(self):
-
+        
+        self.add_track_layer()
         complex_ids = self.find_complexes()
         new_complex_id = np.max(complex_ids) + 1 if complex_ids else 0
         complex_path = Path(self.widget_export_directory.value).joinpath('complexes', f'complex_{new_complex_id}')
