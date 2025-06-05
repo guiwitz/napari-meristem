d2f39fff29a0fa91a9d664a098d6027108820b84
guiwitz <guiwitz@users.noreply.github.com>
2025-06-05 11:51:33 +0200

    update plugin info

--- src/napari_meristem/__init__.py
+++ src/napari_meristem/__init__.py
@@ -5,7 +5,9 @@ except ImportError:
     __version__ = "unknown"
 
 from .meristem_prepoc_widget import MeristemWidget
+from .meristem_select_widget import MeristemAnalyseWidget
 
 __all__ = (
     "MeristemWidget",
+    "MeristemAnalyseWidget",
 )
