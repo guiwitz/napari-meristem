3c503f0e443e1f98dd0fe19f29b9579ba4396eb3
guiwitz <guiwitz@users.noreply.github.com>
2025-06-13 23:40:34 +0200

    add mask fix to widget

--- src/napari_meristem/meristem_select_widget.py
+++ src/napari_meristem/meristem_select_widget.py
@@ -4,9 +4,10 @@ from natsort import natsorted
 from warnings import warn
 import numpy as np
 import pandas as pd
+import skimage
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import (QVBoxLayout, QPushButton,
-                            QWidget, QListWidget, QLabel)
+                            QWidget, QListWidget, QLabel, QMessageBox)
 from napari_guitils.gui_structures import VHGroup, TabSet
 from magicgui.widgets import create_widget
 
@@ -27,7 +28,7 @@ class MeristemAnalyseWidget(QWidget):
         self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
 
-        self.tab_names = ['Tracking', 'Information']
+        self.tab_names = ['Tracking', 'Information', 'Fix Mask']
         self.tabs = TabSet(self.tab_names,
                            tab_layouts=[QVBoxLayout(), QVBoxLayout(), QVBoxLayout()])
         
@@ -120,6 +121,25 @@ class MeristemAnalyseWidget(QWidget):
         self.btn_import_identities = QPushButton('Import identities')
         self.identity_group.glayout.addWidget(self.btn_import_identities, 4, 1, 1, 1)
         self.btn_import_identities.setToolTip("Import identities from a file")
+
+        # Fix mask
+        # manual fix group
+        self.manual_fix_group = VHGroup('Manual fix', orientation='G')
+        self.tabs.add_named_tab('Fix Mask', self.manual_fix_group.gbox)
+        self.btn_init_manual_fix = QPushButton("Initialize manual fix")
+        self.manual_fix_group.glayout.addWidget(self.btn_init_manual_fix, 0, 0, 1, 2)
+        self.btn_init_manual_fix.setToolTip("Initialize manual fix for the selected label")
+        self.btn_update_mask = QPushButton("Update mask after split")
+        self.manual_fix_group.glayout.addWidget(self.btn_update_mask, 1, 0, 1, 2)
+        self.btn_update_mask.setToolTip("Update the mask given manual split")
+        self.btn_update_mask_merged = QPushButton("Update mask after merge")
+        self.manual_fix_group.glayout.addWidget(self.btn_update_mask_merged, 2, 0, 1, 2)
+        self.btn_update_mask_merged.setToolTip("Update the mask given manual merge")
+        self.choice_manual_split_type = create_widget(value='split', 
+            options={"choices": ['split', 'merge'], "label": "Manual split type"})
+        self.manual_fix_group.glayout.addWidget(self.choice_manual_split_type.native, 3, 0, 1, 2)
+
+        self.manual_fix_group.gbox.setMaximumHeight(self.manual_fix_group.gbox.sizeHint().height())
         
         self._add_connections()
 
@@ -143,6 +163,10 @@ class MeristemAnalyseWidget(QWidget):
         self.btn_export_identities.clicked.connect(self._on_export_identities)
         self.btn_import_identities.clicked.connect(self._on_import_identities)
         self.btn_assign_identity.clicked.connect(self._on_assign_identity)
+
+        self.btn_init_manual_fix.clicked.connect(self._on_init_manual_fix)
+        self.btn_update_mask.clicked.connect(self._on_update_after_manual_split)
+        self.btn_update_mask_merged.clicked.connect(self._on_update_after_manual_merge)
 
     
     def _on_add_identity(self):
@@ -503,7 +527,7 @@ class MeristemAnalyseWidget(QWidget):
     
     def _on_import_identities(self):
         """Import identities from a file."""
-        
+
         export_folder = Path(self.widget_export_directory.value)
         export_folder = export_folder.joinpath('identities')
         
@@ -515,3 +539,179 @@ class MeristemAnalyseWidget(QWidget):
         self.identity_list.clear()
         for identity in identities['identity']:
             self.identity_list.addItem(identity)
+
+    # Fix mask methods
+    def _on_load_assembled_data(self):
+        """Load the stitched time series image and mask from the export directory.
+        Optionally load a manually fixed mask if `manual_option` box is ticked"""
+        
+        export_folder = Path(self.widget_export_directory.value)
+
+        if self.manual_option.value:
+            prefix = 'manual_'
+        else:
+            prefix = ''
+
+        im_path = export_folder.joinpath('stitched_image_stack.tif')
+        if not im_path.exists():
+            QMessageBox.critical(self, "Error", f"Stitched image stack does not exist at {im_path}")
+            return
+        
+        self.images_assembled_c = skimage.io.imread(export_folder.joinpath(f'stitched_image_stack.tif'))
+        self.viewer.add_image(self.images_assembled_c, name='stitched_image', colormap='gray', blending='additive')
+
+        if export_folder.joinpath(f'{prefix}stitched_mask_stack.tif').exists():
+            self.masks_assembled_c = skimage.io.imread(export_folder.joinpath(f'{prefix}stitched_mask_stack.tif')).astype(np.uint16)
+            self.viewer.add_labels(self.masks_assembled_c, name='stitched_mask')
+
+    def _on_select_warped_label(self, event):
+        """Triggered upon selecting a cell in the warped mask layer. Creates a single cell 
+        mask of the same label from the stitched mask. Sets the mode to draw for manual split."""
+        
+        selected_label = self.viewer.layers['warped_mask'].selected_label
+        fix_mask = np.zeros_like(self.viewer.layers['stitched_mask'].data[0], dtype=np.uint16)
+        t = self.viewer.dims.current_step[0]
+        fix_mask[self.viewer.layers['stitched_mask'].data[t] == selected_label] = 1
+        self._reset_fix_mask(mask=fix_mask)
+        self.set_split_mode_draw() 
+
+    def _on_init_manual_fix(self):
+        """Initialize the manual fix mode by loading the warped mask and stitched mask layers,
+        and setting the mode to split or merge based on the choice of manual split type. Connects
+        the warped_mask to _on_select_warped_label."""
+        
+        if 'warped_mask' not in self.viewer.layers:
+            self._on_load_warped_data()
+        if 'stitched_mask' not in self.viewer.layers:
+            self._on_load_assembled_data()
+        
+        self.viewer.layers['warped_mask'].events.selected_label.connect(self._on_select_warped_label)
+        self._reset_fix_mask()
+        if self.choice_manual_split_type.value == 'split':
+            self.set_split_mode_select()
+        elif self.choice_manual_split_type.value == 'merge':
+            self.set_merge_mode_draw()
+
+    def _reset_fix_mask(self, mask=None):
+        """Reset the fix mask layer to mask or an empty mask if none is provided."""
+
+        if 'stitched_mask' not in self.viewer.layers:
+            QMessageBox.critical(self, "Error", "Please load stitched mask first")
+            return
+        
+        if mask is None:
+            fix_mask = np.zeros_like(self.viewer.layers['stitched_mask'].data[0], dtype=np.uint16)
+        else:
+            fix_mask = mask
+
+        if 'fix_mask' in self.viewer.layers:
+            self.viewer.layers['fix_mask'].data = fix_mask
+            self.viewer.layers['fix_mask'].refresh()
+        else:
+            self.viewer.add_labels(fix_mask, name='fix_mask')
+
+    def unselect_all_layers(self):
+        """Unselect all layers in the viewer."""
+
+        for layer in self.viewer.layers:
+            layer.selected = False
+
+    def _on_load_ref_points(self):
+        """Load reference points from a CSV file in the export directory and add them to the viewer."""
+        
+        export_folder = Path(self.widget_export_directory.value)
+        self.ref_points = pd.read_csv(export_folder.joinpath('match_points.csv')).values
+        #self.viewer.add_points(name='match_points',ndim=3)
+        #self.viewer.layers['match_points'].data = self.ref_points
+
+    def set_split_mode_draw(self):
+        """Set the viewer to be ready to split a single cell label."""
+
+        self.unselect_all_layers()
+
+        self.viewer.layers['warped_mask'].visible = False
+        self.viewer.layers['warped_image'].visible = False
+        self.viewer.layers['stitched_mask'].visible = False
+        self.viewer.layers['stitched_image'].visible = True
+        self.viewer.layers['fix_mask'].mode = 'erase'
+        self.viewer.layers['fix_mask'].brush_size = 2 
+        self.viewer.layers['fix_mask'].refresh()
+        self.viewer.layers.selection.active = self.viewer.layers['fix_mask']
+
+    def set_split_mode_select(self):
+        """Set the viewer to be ready to select a single cell label for splitting."""
+
+        self.viewer.layers['warped_mask'].visible = True
+        self.viewer.layers['warped_image'].visible = True
+        self.viewer.layers['stitched_mask'].visible = False
+        self.viewer.layers['stitched_image'].visible = False
+        self.unselect_all_layers()
+        self.viewer.layers['warped_mask'].mode = 'pick'
+        self.viewer.layers.selection.active = self.viewer.layers['warped_mask']
+
+    def set_merge_mode_draw(self):
+        """Set the viewer to be ready to merge multiple cell labels into one by
+        drawing over them with the fix mask layer."""
+
+        self.viewer.layers['fix_mask'].mode = 'paint'
+        self.viewer.layers['fix_mask'].brush_size = 2 
+        self.viewer.layers['fix_mask'].refresh()
+
+        self.viewer.layers['warped_mask'].visible = True
+        self.viewer.layers['warped_image'].visible = True
+        self.viewer.layers['stitched_mask'].visible = False
+        self.viewer.layers['stitched_image'].visible = False
+        for layer in self.viewer.layers:
+            layer.selected = False
+        self.viewer.layers.selection.active = self.viewer.layers['fix_mask']
+
+
+    def _on_update_after_manual_split(self):
+        """Assign labels to the two new splits, expand them to fill space up to 
+        the size of the initial mask, and re-warp the mask to fix the warped mask."""
+
+        original_mask = np.zeros_like(self.viewer.layers['stitched_mask'].data[0])
+        sel_label = self.viewer.layers['warped_mask'].selected_label
+        current_mask = self.viewer.layers['stitched_mask'].data[self.viewer.dims.current_step[0]]
+        original_mask[current_mask == sel_label] = 1
+
+        replace_mask = self.viewer.layers['fix_mask'].data
+        replace_mask = skimage.measure.label(replace_mask).astype(np.uint16)
+        replace_mask = skimage.segmentation.expand_labels(replace_mask, distance=10)
+        replace_mask = replace_mask * original_mask
+        
+        replace_mask[replace_mask > 0] = replace_mask[replace_mask > 0] + current_mask.max()
+        current_mask[replace_mask > 0] = replace_mask[replace_mask > 0]
+
+        self._on_load_ref_points()
+        tps_series = preprocess.get_tps_series(ref_points=self.ref_points)
+
+        warped_update = preprocess.warp_single_time(current_mask, tps_series, time=self.viewer.dims.current_step[0], image_type='mask')
+        self.viewer.layers['warped_mask'].data[self.viewer.dims.current_step[0]] = warped_update
+
+        skimage.io.imsave(Path(self.widget_export_directory.value).joinpath(f'manual_warped_mask_stack.tif'), self.viewer.layers['warped_mask'].data)
+        skimage.io.imsave(Path(self.widget_export_directory.value).joinpath(f'manual_stitched_mask_stack.tif'), self.viewer.layers['stitched_mask'].data)
+
+        self._reset_fix_mask()
+        self.set_split_mode_select()
+
+    def _on_update_after_manual_merge(self):
+        """Merge the selected labels in the warped mask and stitched masks."""
+
+        mask_t = self.viewer.layers['warped_mask'].data[self.viewer.dims.current_step[0]]
+        mask_stitched_t = self.viewer.layers['stitched_mask'].data[self.viewer.dims.current_step[0]]
+        to_merge = np.unique(mask_t[self.viewer.layers['fix_mask'].data > 0])
+        for lab in to_merge:
+            mask_t[mask_t == lab] = to_merge[0]
+            mask_stitched_t[mask_stitched_t == lab] = to_merge[0]
+        self.viewer.layers['warped_mask'].refresh()
+        self.viewer.layers['stitched_mask'].refresh()                                
+
+        self.viewer.layers['fix_mask'].data = np.zeros_like(self.viewer.layers['fix_mask'].data)
+        self.viewer.layers['fix_mask'].refresh()
+
+        skimage.io.imsave(Path(self.widget_export_directory.value).joinpath(f'manual_warped_mask_stack.tif'), self.viewer.layers['warped_mask'].data)
+        skimage.io.imsave(Path(self.widget_export_directory.value).joinpath(f'manual_stitched_mask_stack.tif'), self.viewer.layers['stitched_mask'].data)
+
+        self._reset_fix_mask()
+        self.set_merge_mode_draw()
